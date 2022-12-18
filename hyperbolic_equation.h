5f1bce48249911893b63f4d6a981461078040d5f
bibatn <habibulinmarat@icloud.com>
2022-12-18 16:42:41 +0300

    Убрана лишняя структура.

--- hyperbolic_equation.h
+++ hyperbolic_equation.h
@@ -71,11 +71,6 @@ struct Grid
     int K;
 };
 
-enum Axis
-{
-    X, Y, Z
-};
-
 
 class Functions
 {
@@ -114,7 +109,7 @@ public:
 
 };
 
-void split(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max, int size, Axis axis, std::vector<Block> &blocks)
+void split(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max, int size, char axis, std::vector<Block> &blocks)
 {
     // split the grid recursively into blocks
     if (size == 1)
@@ -125,49 +120,49 @@ void split(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max, int
 
     if (size % 2 == 1)
     { // if size is odd we make it even
-        if (axis == X)
+        if (axis == 'X')
         {
             int x = x_min + (x_max - x_min) / size;
             blocks.emplace_back(x_min, x, y_min, y_max, z_min, z_max);
             x_min = x + 1;
-            axis = Y;
+            axis = 'Y';
         }
-        else if (axis == Y)
+        else if (axis == 'Y')
         {
             int y = y_min + (y_max - y_min) / size;
             blocks.emplace_back(x_min, x_max, y_min, y, z_min, z_max);
             y_min = y + 1;
-            axis = Z;
+            axis = 'Z';
         }
         else
         {
             int z = z_min + (z_max - z_min) / size;
             blocks.emplace_back(x_min, x_max, y_min, y_max, z_min, z);
             z_min = z + 1;
-            axis = X;
+            axis = 'X';
         }
 
         size--;
     }
 
     // now the size is even
-    if (axis == X)
+    if (axis == 'X')
     {
         int x = (x_min + x_max) / 2;
-        split(x_min, x, y_min, y_max, z_min, z_max, size / 2, Y, blocks);
-        split(x + 1, x_max, y_min, y_max, z_min, z_max, size / 2, Y, blocks);
+        split(x_min, x, y_min, y_max, z_min, z_max, size / 2, 'Y', blocks);
+        split(x + 1, x_max, y_min, y_max, z_min, z_max, size / 2, 'Y', blocks);
     }
-    else if (axis == Y)
+    else if (axis == 'Y')
     {
         int y = (y_min + y_max) / 2;
-        split(x_min, x_max, y_min, y, z_min, z_max, size / 2, Z, blocks);
-        split(x_min, x_max, y + 1, y_max, z_min, z_max, size / 2, Z, blocks);
+        split(x_min, x_max, y_min, y, z_min, z_max, size / 2, 'Z', blocks);
+        split(x_min, x_max, y + 1, y_max, z_min, z_max, size / 2, 'Z', blocks);
     }
     else
     {
         int z = (z_min + z_max) / 2;
-        split(x_min, x_max, y_min, y_max, z_min, z, size / 2, X, blocks);
-        split(x_min, x_max, y_min, y_max, z + 1, z_max, size / 2, X, blocks);
+        split(x_min, x_max, y_min, y_max, z_min, z, size / 2, 'X', blocks);
+        split(x_min, x_max, y_min, y_max, z + 1, z_max, size / 2, 'X', blocks);
     }
 }
 
@@ -428,7 +423,7 @@ public:
     {
         // split grid between processes
         std::vector<Block> blocks;
-        split(0, g.N, 0, g.N, 0, g.N, proc_size, X, blocks);
+        split(0, g.N, 0, g.N, 0, g.N, proc_size, 'X', blocks);
         Block block = blocks[proc_rank];
 //        block.expand_Block();
 
