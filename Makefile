5b35bb7eee33576d0430506e9ff30cbeafe179c5
bibatn <habibulinmarat@icloud.com>
2022-12-18 13:42:03 +0300

    Небольшие правки.

--- Makefile
+++ Makefile
@@ -16,7 +16,6 @@ submit-polus-parallel:
 		for p in 1 4 16; do \
 			for i in {1..2} ; do \
 				bsub -n $$p -m polus-c3-ib -W 00:10 -o /dev/null -e /dev/null OMP_NUM_THREADS=1 mpiexec ./hyperbolic_equation $$N 1 out\_$$p\_$$N\_1.txt ; \
-				bsub -n $$p -m polus-c3-ib -W 00:10 -o /dev/null -e /dev/null OMP_NUM_THREADS=1 mpiexec ./hyperbolic_equation $$N out\_$$p\_$$N\_pi.txt ; \
 			done \
 		done \
 	done
--- hyperbolic_equation.h
+++ hyperbolic_equation.h
@@ -4,53 +4,10 @@
 #include <mpi.h>
 #include <stdexcept>
 
-enum Axis
-{
-    X, Y, Z
-};
-struct Grid
-{
-    double L_x, L_y, L_z; // spatial sizes
-    double h_x, h_y, h_z; // spatial steps
-    int N; // number of spatial points
-
-    double T; // time range
-    double tau; // time step
-    int K; // number of time points
-
-    Grid(double L_x, double L_y, double L_z, int N, double T, int K)
-    {
-        this->L_x = L_x;
-        this->L_y = L_y;
-        this->L_z = L_z;
-        this->N = N;
-
-        this->T = T;
-        this->K = K;
-
-        this->h_x = L_x / N;
-        this->h_y = L_y / N;
-        this->h_z = L_z / N;
-
-        this->tau = T / K;
-    }
-};
-
 struct Block
 {
-    int x_min;
-    int x_max;
-    int y_min;
-    int y_max;
-    int z_min;
-    int z_max;
-    int x_size;
-    int y_size;
-    int z_size;
-    int size;
-
     Block(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max) :
-    x_min(x_min), x_max(x_max), y_min(y_min), y_max(y_max), z_min(z_min), z_max(z_max)
+            x_min(x_min), x_max(x_max), y_min(y_min), y_max(y_max), z_min(z_min), z_max(z_max)
     {
         x_size = x_max - x_min + 1;
         y_size = y_max - y_min + 1;
@@ -79,7 +36,46 @@ struct Block
         z_size = z_max - z_min + 1;
         size = x_size * y_size * z_size;
     }
+
+    int x_min, y_min, z_min;
+    int x_max, y_max, z_max;
+    int x_size, y_size, z_size;
+    int size;
 };
+
+struct Grid
+{
+    Grid(double L_x, double L_y, double L_z, int N, double T, int K)
+    {
+        this->L_x = L_x;
+        this->L_y = L_y;
+        this->L_z = L_z;
+        this->N = N;
+
+        this->T = T;
+        this->K = K;
+
+        this->h_x = L_x / N;
+        this->h_y = L_y / N;
+        this->h_z = L_z / N;
+
+        this->tau = T / K;
+    }
+
+    double L_x, L_y, L_z;
+    double h_x, h_y, h_z;
+    int N;
+
+    double T;
+    double tau;
+    int K;
+};
+
+enum Axis
+{
+    X, Y, Z
+};
+
 
 class Functions
 {
