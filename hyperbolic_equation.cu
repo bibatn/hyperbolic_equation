68d4d46badb85ffd674de810e82be420739c5e34
bibatn <habibulinmarat@icloud.com>
2022-12-26 15:47:41 +0300

    test25

--- hyperbolic_equation.cu
+++ hyperbolic_equation.cu
@@ -4,11 +4,6 @@
 #include <cmath>
 #include <mpi.h>
 #include <fstream>
-#ifdef __CUDACC__
-#define CUDA_HOSTDEV __host__ __device__
-#else
-#define CUDA_HOSTDEV
-#endif
 
 double Functions::AnalyticalSolution(double x, double y, double z, double t) const
 {
